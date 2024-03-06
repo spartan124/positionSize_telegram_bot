959790cf2b41856523b8d2926dbf43e4bda3d18e
Jeremiah Ojonuba <ojonuba93@gmail.com>
2024-03-06 18:28:52 +0100

    added get req

--- index.js
+++ index.js
@@ -26,6 +26,9 @@ const app = express()
 const PORT = process.env.PORT || 3000
 
 app.use(express.json())
+app.get(`/webhook/${botToken}`, (req, res) => {
+  res.sendStatus(200);
+});
 
 app.post(`/webhook/${botToken}`, (req, res) => {
   // const updates = req.body
